2a324ceeb30ce26c9d589ca86fc43bfbbbcc22d8
Hanming Zhu <hanming@huawen.com>
2021-09-26 12:01:41 +0800

    [Misc] Rewrite createNewYml as createNewJson

--- ta-scripts/update-students-from-coursemology/createNewJson.js
+++ ta-scripts/update-students-from-coursemology/createNewJson.js
@@ -1,10 +1,12 @@
 /*
   Developed by John Phua, 2020
   To run this script, run:
-  node createNewYml.js
+  node createNewJson.js
 
-  It will generate a new student.yml in the same folder for you to replace in `_data`.
-  It will also create a <year> folder with the images of students to add to `img/students`.
+  It will generate a new `student.json` in the current folder (where this .js file is)
+  for you to use in `website/students`.
+  It will also create a <year> folder with the images of students to add to
+  `website/static/img/students`.
 
   Expects the Coursemology answer zip to take on the following format:
   /<dataDir>
@@ -18,21 +20,26 @@
       /<PHOTO_FOLDER>
         /<photo>.jpg
     /... other students
+  
+  (By default, dataDir is defined as `/data/submissions`)
 */
+
 const slugify = require('slugify');
 const fs = require('fs');
 const path = require('path');
 const { htmlToText } = require('html-to-text');
 
-// Unless yml format or dir change, there should be no need to modify this
-var originalFileDir = '../../_data/students.yml';
 // Directory containing folders of user submissions downloaded from Coursemology (expects parent directory of user folders)
 const dataDir = 'data/submissions';
+// Unless json format or dir change, there should be no need to modify this
+const originalFileDir = '../../website/students/students.json';
 
+// Subfolders for each question
 const ONELINER_FOLDER = 'Question 1 Give us a one-line description of yourself';
 const LINK_FOLDER = 'Question 2 Link (URL) for your name';
 const FACULTY_FOLDER = 'Question 3 Faculty';
 const PHOTO_FOLDER = 'Question 4 Please upload a photo of yourself here';
+// For text responses, Coursemology exports them as `answer.txt`.
 const ANSWER_FILE = 'answer.txt';
 
 fs.readFile(originalFileDir, function (err, data) {
@@ -45,55 +52,63 @@ fs.readFile(originalFileDir, function (err, data) {
       return console.error(err);
     }
 
-    let currDate = new Date();
-    let thisYear = (1900 + currDate.getYear()).toString();
-    var oldStr = data.toString();
-    var ymlStr = '- batch:\n  year: ' + thisYear + '\n  students:';
+    const currDate = new Date();
+    const thisYear = currDate.getFullYear();
+    const thisYearFilePath = thisYear.toString();
+    let newYearStudentsObject = { year: thisYear, students: [] };
 
-    if (!fs.existsSync(thisYear)) {
-      fs.mkdirSync(thisYear);
+    if (!fs.existsSync(thisYearFilePath)) {
+      fs.mkdirSync(thisYearFilePath);
     }
 
     userSubmissions
       .filter((entry) => entry.isDirectory())
       .forEach((submission) => {
         copyImageWithRename(
-          thisYear,
+          thisYearFilePath,
           submission.name,
           path.join(dataDir, submission.name)
         );
-        ymlStr += processStudent(
-          submission.name,
-          path.join(dataDir, submission.name)
+        newYearStudentsObject.students.push(
+          processStudent(submission.name, path.join(dataDir, submission.name))
         );
       });
 
-    ymlStr += '\n' + oldStr;
-    fs.writeFile(`students.yml`, ymlStr, function (err) {
-      if (err) {
-        return console.error(err);
+    const oldStudentsArray = JSON.parse(data).filter(
+      (obj) => obj.year !== thisYear
+    );
+    const newStudentsArray = [newYearStudentsObject, ...oldStudentsArray].sort(
+      (a, b) => b.year - a.year
+    );
+    fs.writeFile(
+      `students.json`,
+      JSON.stringify(newStudentsArray, null, 2),
+      function (err) {
+        if (err) {
+          return console.error(err);
+        }
       }
-    });
+    );
   });
 });
 
 function processStudent(studentName, studentDirName) {
   const imageID = formImageName(studentName);
-  let studentStr = `\n  - name: ${studentName.trim()}`;
-  studentStr += `\n    id: ${imageID}`;
-  studentStr += processStudentFaculty(studentDirName);
-  studentStr += processStudentLink(studentDirName);
-  studentStr += processStudentOneLiner(studentDirName);
-  return studentStr;
+  const studentObject = {
+    name: studentName.trim(),
+    id: imageID,
+    faculty: processStudentFaculty(studentDirName),
+    blog: processStudentLink(studentDirName),
+    one_liner: processStudentOneLiner(studentDirName),
+  };
+  return studentObject;
 }
 
 function processStudentOneLiner(studentDirName) {
   const file = path.join(studentDirName, ONELINER_FOLDER, ANSWER_FILE);
 
   if (fs.existsSync(file)) {
-    return `\n    one_liner: "${parseHtmlToText(
-      fs.readFileSync(file, 'utf8')
-    ).trim()}"`;
+    return parseHtmlToText(fs.readFileSync(file, 'utf8')).trim();
   } else {
     console.log('One-liner not found in ' + studentDirName);
     return '';
@@ -104,9 +119,7 @@ function processStudentLink(studentDirName) {
   const file = path.join(studentDirName, LINK_FOLDER, ANSWER_FILE);
 
   if (fs.existsSync(file)) {
-    return `\n    blog: "${parseHtmlToText(
-      fs.readFileSync(file, 'utf8')
-    ).trim()}"`;
+    return parseHtmlToText(fs.readFileSync(file, 'utf8')).trim();
   } else {
     console.log('Link not found in ' + studentDirName);
     return '';
@@ -117,9 +130,7 @@ function processStudentFaculty(studentDirName) {
   const file = path.join(studentDirName, FACULTY_FOLDER, ANSWER_FILE);
 
   if (fs.existsSync(file)) {
-    return `\n    faculty: "${parseHtmlToText(
-      fs.readFileSync(file, 'utf8')
-    ).trim()}"`;
+    return parseHtmlToText(fs.readFileSync(file, 'utf8')).trim();
   } else {
     console.log('Faculty not found in ' + studentDirName);
     return '';
@@ -151,7 +162,7 @@ function formImageName(studentName) {
 }
 
 function parseHtmlToText(htmlString) {
-  return htmlToText.fromString(htmlString, {
+  return htmlToText(htmlString, {
     ignoreHref: true,
     wordwrap: false,
     singleNewLineParagraphs: true,
