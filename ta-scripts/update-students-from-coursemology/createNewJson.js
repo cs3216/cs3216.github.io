ccb31748ee98e03affc45de9c0d9ebe2ee5d2977
Christopher Goh <chrisgzf@gmail.com>
2022-08-08 05:13:42 +0800

    [Misc] Update coursemology update script to clean up faculty and URLs

--- ta-scripts/update-students-from-coursemology/createNewJson.js
+++ ta-scripts/update-students-from-coursemology/createNewJson.js
@@ -24,23 +24,23 @@
   (By default, dataDir is defined as `/data/submissions`)
 */
 
-const slugify = require('slugify');
-const fs = require('fs');
-const path = require('path');
-const { htmlToText } = require('html-to-text');
+const slugify = require("slugify");
+const fs = require("fs");
+const path = require("path");
+const { htmlToText } = require("html-to-text");
 
 // Directory containing folders of user submissions downloaded from Coursemology (expects parent directory of user folders)
-const dataDir = 'data/submissions';
+const dataDir = "data/submissions";
 // Unless json format or dir change, there should be no need to modify this
-const originalFileDir = '../../website/students/students.json';
+const originalFileDir = "../../website/students/students.json";
 
 // Subfolders for each question
-const ONELINER_FOLDER = 'Question 1 Give us a one-line description of yourself';
-const LINK_FOLDER = 'Question 2 Link (URL) for your name';
-const FACULTY_FOLDER = 'Question 3 Faculty';
-const PHOTO_FOLDER = 'Question 4 Please upload a photo of yourself here';
+const ONELINER_FOLDER = "Question 1 Give us a one-line description of yourself";
+const LINK_FOLDER = "Question 2 Link (URL) for your name";
+const FACULTY_FOLDER = "Question 3 Faculty";
+const PHOTO_FOLDER = "Question 4 Please upload a photo of yourself here";
 // For text responses, Coursemology exports them as `answer.txt`.
-const ANSWER_FILE = 'answer.txt';
+const ANSWER_FILE = "answer.txt";
 
 fs.readFile(originalFileDir, function (err, data) {
   if (err) {
@@ -109,11 +109,11 @@ function processStudentOneLiner(studentDirName) {
 
   if (fs.existsSync(file)) {
     return standardiseCharacters(
-      parseHtmlToText(fs.readFileSync(file, 'utf8'))
+      parseHtmlToText(fs.readFileSync(file, "utf8"))
     );
   } else {
-    console.log('One-liner not found in ' + studentDirName);
-    return '';
+    console.log("One-liner not found in " + studentDirName);
+    return "";
   }
 }
 
@@ -121,10 +121,17 @@ function processStudentLink(studentDirName) {
   const file = path.join(studentDirName, LINK_FOLDER, ANSWER_FILE);
 
   if (fs.existsSync(file)) {
-    return parseHtmlToText(fs.readFileSync(file, 'utf8'));
+    const rawLink = parseHtmlToText(fs.readFileSync(file, "utf8"));
+    if (!rawLink.startsWith("http://") && !rawLink.startsWith("https://")) {
+      console.log(
+        `${studentDirName} link does not start with http(s), prefixing ${rawLink} with 'https://'.`
+      );
+      return "https://" + rawLink;
+    }
+    return rawLink;
   } else {
-    console.log('Link not found in ' + studentDirName);
-    return '';
+    console.log("Link not found in " + studentDirName);
+    return "";
   }
 }
 
@@ -132,10 +139,14 @@ function processStudentFaculty(studentDirName) {
   const file = path.join(studentDirName, FACULTY_FOLDER, ANSWER_FILE);
 
   if (fs.existsSync(file)) {
-    return parseHtmlToText(fs.readFileSync(file, 'utf8'));
+    const rawFaculty = parseHtmlToText(fs.readFileSync(file, "utf8"));
+    return rawFaculty
+      .replace(/^Computer Science$/i, "Computing")
+      .replace(/^School of Computing$/i, "Computing")
+      .replace(/^SoC$/i, "Computing");
   } else {
-    console.log('Faculty not found in ' + studentDirName);
-    return '';
+    console.log("Faculty not found in " + studentDirName);
+    return "";
   }
 }
 
@@ -143,7 +154,7 @@ function copyImageWithRename(targetFolder, studentName, studentDirName) {
   const dirPath = path.join(studentDirName, PHOTO_FOLDER);
   fs.readdir(dirPath, (err, files) => {
     if (err || files.length === 0) {
-      return console.log('Image not found for ' + studentName, err);
+      return console.log("Image not found for " + studentName, err);
     }
 
     const imagePath = path.join(dirPath, files[0]);
@@ -153,7 +164,7 @@ function copyImageWithRename(targetFolder, studentName, studentDirName) {
     );
     fs.copyFile(imagePath, destPath, (err) => {
       if (err) {
-        console.log('Image could not be copied for ' + studentName, err);
+        console.log("Image could not be copied for " + studentName, err);
       }
     });
   });
@@ -161,12 +172,12 @@ function copyImageWithRename(targetFolder, studentName, studentDirName) {
 
 function toTitleCase(words) {
   return words
-    .split(' ')
+    .split(" ")
     .map(
       (word) =>
         `${word.charAt(0).toUpperCase()}${word.substring(1).toLowerCase()}`
     )
-    .join(' ');
+    .join(" ");
 }
 
 function formImageName(studentName) {
@@ -180,18 +191,18 @@ function parseHtmlToText(htmlString) {
     singleNewLineParagraphs: true,
     uppercaseHeadings: false,
   })
-    .replace(/\s/g, ' ')
+    .replace(/\s/g, " ")
     .trim();
 }
 
 function standardiseCharacters(str) {
   return str
-    .replace('”', '"')
-    .replace('“', '"')
-    .replace('’', "'")
-    .replace('‘', "'")
+    .replace("”", '"')
+    .replace("“", '"')
+    .replace("’", "'")
+    .replace("‘", "'")
     .replace(/[\u2018\u2019]/g, "'")
     .replace(/[\u201C\u201D]/g, '"')
-    .replace(/[\u2013\u2014]/g, '-')
-    .replace(/[\u2026]/g, '...');
+    .replace(/[\u2013\u2014]/g, "-")
+    .replace(/[\u2026]/g, "...");
 }
