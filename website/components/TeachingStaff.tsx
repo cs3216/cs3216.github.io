214b21c617c9d2591fbf4ff52d984bf62596cea7
Yangshun <tay.yang.shun@gmail.com>
2023-08-09 16:55:38 +0800

    [Site] Add teaching staff for AY2023/24

--- website/components/TeachingStaff.tsx
+++ website/components/TeachingStaff.tsx
@@ -6,50 +6,37 @@ type Props = Readonly<{
   year: number;
 }>;
 
-export default function StudentList({ hasClassPhoto = true, year }: Props) {
+export default function TeachingStaff({ hasClassPhoto = true, year }: Props) {
   return (
     <div className="py-4">
       <div className="mx-auto max-w-7xl flex flex-col gap-y-8">
-        {hasClassPhoto && (
-          <img
-            alt="Class photo"
-            className="rounded-2xl w-full"
-            src={`/img/class-photos/${year}.jpg`}
-            loading="lazy"
-          />
-        )}
         <ul
           role="list"
-          className="mx-auto grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+          className="mx-auto grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 md:grid-cols-3 lg:mx-0 lg:max-w-none lg:grid-cols-4"
         >
           {students
             .find((batch) => batch.year === year)
-            .students.map((student) => (
-              <li key={student.name}>
+            .staff.map((person) => (
+              <li key={person.name}>
                 <img
                   className="aspect-square w-full rounded-2xl object-cover"
-                  src={`/img/students/${year}/${student.id}.jpg`}
-                  alt={student.name}
+                  src={person.image}
+                  alt={person.name}
                   loading="lazy"
                 />
                 <Link
                   className="hover:underline"
-                  href={student.url}
+                  href={person.url}
                   target="_blank"
                   rel="noreferrer noopener"
                 >
                   <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
-                    {student.name} ↗
+                    {person.name} ↗
                   </h3>
                 </Link>
-                {student.faculty && (
+                {person.role && (
                   <p className="text-base leading-7 text-gray-600 dark:text-gray-400">
-                    {student.faculty}
-                  </p>
-                )}
-                {student.one_liner && (
-                  <p className="mt-4 text-base leading-7 text-gray-800 dark:text-gray-200">
-                    {student.one_liner}
+                    {person.role}
                   </p>
                 )}
               </li>
