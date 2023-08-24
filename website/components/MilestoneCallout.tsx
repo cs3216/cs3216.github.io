127fd60e9722735b86c0c94e3a9a7e5f06b80e05
Yangshun <tay.yang.shun@gmail.com>
2023-08-24 07:20:19 +0300

    [A1] Replace milestone callouts with custom component

--- website/components/MilestoneCallout.tsx
+++ website/components/MilestoneCallout.tsx
@@ -3,16 +3,22 @@ import {ReactNode} from 'react';
 
 export default function MilestoneCallout({
   children,
+  customNumber,
   labelSuffix,
 }: Readonly<{
   children: ReactNode;
-  labelSuffix: ReactNode;
+  customNumber: string; // For odd milestone numbers like "5a" or to start the current milestone from another number.
+  labelSuffix: ReactNode; // Text added behind the "Milestone X" label.
 }>) {
   return (
     <Callout type="info" emoji="🏁">
-      <div className="milestone">
+      <div
+        className="milestone"
+        style={{
+          counterSet: customNumber ? `milestone ${customNumber}` : undefined,
+        }}>
         <strong>
-          Milestone <span className="milestone-counter" />
+          Milestone {customNumber ?? <span className="milestone-counter" />}
           {labelSuffix && <> {labelSuffix}</>}:
         </strong>{' '}
         {children}
